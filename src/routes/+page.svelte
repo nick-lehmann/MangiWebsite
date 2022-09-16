cfbdb7a5051e32d3ff253fb37f0873b2436410d4
Nick Lehmann <nick@lehmann.sh>
2022-09-16 23:35:01 +0200

    Canteen search

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,10 @@
 
 	export let data: PageData
 
+	let needle: string = ''
+
 	let canteens: Canteen[] = []
-	$: canteens = getList(data.canteens, { city })
+	$: canteens = getList(data.canteens, { city }, needle)
 
 	$: cities = Array.from(new Set(data.canteens.map((c) => c.city))).sort()
 	let city: string | undefined = undefined
@@ -14,15 +16,22 @@
 		city: string | undefined
 	}
 
-	function getList(all: Canteen[], filter: CanteenFilter): Canteen[] {
+	function getList(all: Canteen[], filter: CanteenFilter, needle: string): Canteen[] {
 		let canteens = Array.from(all)
 
 		if (filter.city) canteens = canteens.filter((c) => c.city === filter.city)
+
+		if (needle !== '') {
+			const searchTerm = needle.toLowerCase()
+			canteens = canteens.filter((canteen) => canteen.name.toLowerCase().includes(searchTerm))
+		}
 
 		canteens.sort((a, b) => a.name.localeCompare(b.name))
 		return canteens
 	}
 </script>
+
+<input type="text" bind:value={needle} />
 
 <p>
 	City
